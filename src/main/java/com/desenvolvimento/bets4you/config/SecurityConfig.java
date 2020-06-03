5730ebbfe04f276ac018a08619b0f2ef27b0c90d
Breno <ebrenocn@gmail.com>
2020-06-03 16:23:15 -0300

    testing integrate mercado pago

--- src/main/java/com/desenvolvimento/bets4you/config/SecurityConfig.java
+++ src/main/java/com/desenvolvimento/bets4you/config/SecurityConfig.java
@@ -44,8 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.DELETE, "/aposta/**")
 		.antMatchers(HttpMethod.DELETE, "/equipe/**")
 		.antMatchers("/gestao-de-banca")
-		.antMatchers("/como-trabalhamos");
-
+		.antMatchers("/como-trabalhamos")
+		.antMatchers(HttpMethod.POST, "/mercadopago-compra");
 	}
 	
 	@Override
