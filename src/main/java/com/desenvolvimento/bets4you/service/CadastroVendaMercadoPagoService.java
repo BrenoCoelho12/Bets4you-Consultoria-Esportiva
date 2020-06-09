34ea81e96b3570bf3580cb655b6ca9e52d081511
Breno <ebrenocn@gmail.com>
2020-06-09 10:51:22 -0300

    config access token and public key producao mercado pago

--- src/main/java/com/desenvolvimento/bets4you/service/CadastroVendaMercadoPagoService.java
+++ src/main/java/com/desenvolvimento/bets4you/service/CadastroVendaMercadoPagoService.java
@@ -38,7 +38,7 @@ public class CadastroVendaMercadoPagoService {
             Float valorPlano = plano.get().getValorTotal().floatValue();
 
 
-            MercadoPago.SDK.setAccessToken("TEST-3355084366349304-060401-2dad242761eda211677ed598ecb814ae-568143852");
+            MercadoPago.SDK.setAccessToken("APP_USR-3355084366349304-060401-d0d9c74d2a38416856746341d78cf07a-568143852");
 
             Payment payment = new Payment();
             payment
