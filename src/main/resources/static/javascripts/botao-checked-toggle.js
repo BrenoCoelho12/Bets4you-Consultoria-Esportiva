d4c5267cfe8dc9949a9200bc4ca727178e043272
Breno <ebrenocn@gmail.com>
2020-07-03 23:28:25 -0300

    updating view manegement bets

--- src/main/resources/static/javascripts/botao-checked-toggle.js
+++ src/main/resources/static/javascripts/botao-checked-toggle.js
@@ -8,6 +8,7 @@ Bets4you.ButtonCheckedToggle = (function(){
 
     ButtonCheckedToggle.prototype.iniciar = function() {
         $('table thead ').on('change', '.toggle-event', function(){
+            console.log("entrou botao status.")
             var rowCurrent = $(this).closest('tr'); //pegando a linha selecionada
 
             /*pegando os valores das colunas*/
@@ -43,7 +44,46 @@ Bets4you.ButtonCheckedToggle = (function(){
             }
         });
 
+        $('table thead ').on('change', '.toggle-event-situacao', function(){
+            console.log("entrou botao situacao");
+            var rowCurrent = $(this).closest('tr'); //pegando a linha selecionada
+
+            /*pegando os valores das colunas*/
+            var idAposta = rowCurrent.find("td").eq(0).text();
+
+
+            if($(this).prop('checked')){
+                /*se o checkbox for selecionado enviar um ajax passando o id da aposta e, também, o status da aposta como TRUE*/
+                $.ajax({
+                    url: '/aposta/atualizarSituacao',
+                    method: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify(
+                        { 'id': idAposta,
+                            'situacao': 'VENCIDA'
+                        }),
+                    beforeSend: adicionarCsrfToken
+                });
+            }
+
+            else {
+                /*se o checkbox for desativado enviar um ajax passando o id da aposta e, também, o status da aposta como FALSE*/
+                $.ajax({
+                    url: '/aposta/atualizarSituacao',
+                    method: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify(
+                        { 'id': idAposta,
+                            'situacao': 'PERDIDA'
+                        }),
+                    beforeSend: adicionarCsrfToken
+                });
+            }
+        });
+
     }
+
+
 
     function adicionarCsrfToken(xhr) {
         var token = $('input[name=_csrf]').val();
